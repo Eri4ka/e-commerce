493fa2d5cdd6c4eeacde0d07d36ed54b0a942aa4
Erik Hasanov <navyhasanov@gmail.com>
2022-11-07 22:15:20 +0500

    fix express path

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ server.use(router);
 server.listen(PORT, () => {
   console.log('Server is running');
 });
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './build/index.html'));
