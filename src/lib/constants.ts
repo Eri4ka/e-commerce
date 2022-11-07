b6c898820cc5426d685778482f8719a7fc61aded
Erik Hasanov <navyhasanov@gmail.com>
2022-11-07 20:07:03 +0500

    heroku fix 3

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const MOBILE_VIEW = 991;
 export const ITEMS_PER_PAGE = 6;
-export const BASE_URL = 'http://localhost:3001';
+// export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://e-commerce-eri.herokuapp.com/api';
